b8a414004cdc5f8ca87b1de36d8f2a1e51b75368
kolanaveen <kolanaveen8374@gmail.com>
2026-04-02 11:40:03 +0530

    Initial commit: Learning

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// testing
 Route::get('/', fn() => view('home'))->name('home');
 Route::get('/about', fn() => view('about'))->name('about');
 Route::get('/skills', fn() => view('skills'))->name('skills');
